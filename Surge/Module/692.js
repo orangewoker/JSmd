925c08e2a3b28eae55d32aef63dce8e07c4dce8a
orangewoker <60803065+orangewoker@users.noreply.github.com>
2021-02-24 14:47:43 +0800

    Create 692.js

---
+++ Surge/Module/692.js
@@ -0,0 +1,15 @@
+var obj = JSON.parse($response.body);
+
+if ($request.url.indexOf("/user/personal") != -1){
+
+    obj.data.vip = 1;
+    obj.data.vip_time = "2099-09-09 09:09:09";
+    obj.data.nickname = "ios黑科技";
+    obj.data.vip_ymd = "2099-09-09";
+    obj.data.huancun_count = 5201314;
+    
+}
+
+
+
+$done({body: JSON.stringify(obj)});
